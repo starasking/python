a4dff028d05f020b3cca063ef2e1a77ebf6ae5a3
wangxuemei <wangxuemei@gotokeep.com>
2019-06-24 17:06:52 +0800

    succeed

--- gbdt.py
+++ gbdt.py
@@ -26,130 +26,90 @@ query = pd.DataFrame(np.array([['Sunny', 'Hot', 'Normal', 'Weak']]),
                      columns = ['Outlook', 'Temp', 'Humididty', 'Wind'])
 
 class Tree:
-    def __init__(self, entropy, value, split_feature, character):
+    def __init__(self, entropy, value, split_feature):
         self.entropy = entropy
         self.value = value
         self.feature = split_feature
-        self.character = character
         self.branches = {}
 
-class Node:
-    def __init__(self, character, entropy, value):
-        self.entropy = entropy
-        self.value = value
-        self.character = character
 
 def split_branch(df, output_col):
     entropy_pre = df[output_col].std(ddof=0)
+    value = df[output_col].mean()
     features = list(df.columns)
     features.remove(output_col)
 
     # TODO: add stop condition
     if len(features) == 0 or entropy_pre < Entropy_threshold:
-        return ("", None)
+        return Tree(entropy_pre, value, "")
+
     split_feature = features[0]
-    return_nodes = []
+    return_characters = []
     gain = 0
 
     for col in features:
-        characters = set(df[col])
+        character_set = set(df[col])
         denominator = float(df.shape[0])
         entropy = 0
-        nodes = []
-        for c in characters:
+        characters = []
+        for c in character_set:
             numerator = float(df[df[col] == c].shape[0])
             std = df[df[col] == c][output_col].std(ddof=0)
-            value = df[df[col] == c][output_col].mean()
             entropy += numerator/denominator * std
-            nodes.append(Node(c, std, value))
+            characters.append(c)
 
         if gain < entropy_pre - entropy :
             gain = entropy_pre - entropy
             split_feature = col
-            return_nodes = nodes
+            return_characters = characters
 
-    return (split_feature, return_nodes)
+    tree = Tree(entropy_pre, value, split_feature)
+    for character in return_characters:
+        tree.branches[character] = None #Tree(0.0, 0.0, "")
+    return tree
 
-def grow_tree(df, split_feature, nodes, tree, leaves):
-    for node in nodes:
-        subtree = Tree(node.entropy, node.value, "", node.character)
-        tree.branches[node.character] = subtree
-        print(split_feature)
-        print(tree.branches)
-        subdf = df[df[split_feature] == node.character]
-        del subdf[split_feature]
+def grow_tree(df, output_col, leaves):
+    tree = split_branch(df, output_col)
+    if tree.feature == "":
+        return (tree, leaves)
+    for character in tree.branches.keys():
+        subdf = df[df[tree.feature] == character]
+        del subdf[tree.feature]
+        subtree = split_branch(subdf, output_col)
+        tree.branches[character] = subtree
         leaves[subtree] = subdf
     return (tree, leaves)
 
 def create_tree(df, output_col):
-    entropy = df[output_col].std(ddof=0)
-    value = df[output_col].mean()
-    (split_feature, nodes) = split_branch(df, output_col)
-    tree = Tree(entropy, value, split_feature, "")
     leaves = {}
-
+    (tree, leaves) = grow_tree(df, output_col, leaves)
+    print("=============================")
     print(df)
-    print(split_feature)
+    print("-----------------------------")
+    print(tree.entropy)
+    print(tree.value)
+    print(tree.feature)
 
-    for node in nodes:
-        print(node.entropy, node.character)
-    (new_tree, new_leaves) = grow_tree(df, split_feature, nodes, tree, leaves)
-    tree = new_tree
-    leaves = new_leaves
-
-    #print("=============================")
-    #print(tree.value)
-    #print(tree.entropy)
-    #print(tree.feature)
-    #print(tree.branches)
-    #print("=============================")
-    while bool(leaves):
-        subtree = list(leaves.keys())[0]
-        df = leaves[subtree]
-        (split_feature, nodes) = split_branch(df, output_col)
-        subtree.feature = split_feature
-        #print("=============================")
-        #print(subtree.value)
-        #print(subtree.entropy)
-        #print(subtree.feature)
-        #print(subtree.branches)
-        #print("=============================")
-        #input("pause")
-        if split_feature == "":
-            del leaves[subtree]
-            continue
-        print(df)
-        print(split_feature)
-        for node in nodes:
-            print(node.entropy, node.character)
-        (new_tree, new_leaves) = grow_tree(df, split_feature, nodes, subtree, leaves)
-        del leaves[subtree]
-        subtree = new_tree
-        leaves = new_leaves
+    for character in tree.branches.keys():
+        print(character)
+        subtree = tree.branches[character]
+        subdf = leaves[subtree]
+        newtree = create_tree(subdf, output_col)
+        tree.branches[character] = newtree 
     return tree
 
 tree = create_tree(df, "Decision")
 
+
+print(df)
 def predict(query, tree):
     while query.shape[1] > 0:
         current_feature = tree.feature
-        if query[tree.feature][0] not in tree.branches.keys():
+        if current_feature == "" or query[tree.feature][0] not in tree.branches.keys():
             return tree.value
         tree = tree.branches[query[tree.feature][0]]
         del query[current_feature]
     return tree.value
-
-#def print_tree(tree):
-    #print("=============================")
-    #print(tree.value)
-    #print(tree.entropy)
-    #print(tree.feature)
-    #print(tree.branches)
-
-#print_tree(tree)
-#print_tree(tree.branches['Sunny'])
-#print(df)
-#print(query)
-
 print("=============================")
+print(query)
 print(predict(query, tree))
